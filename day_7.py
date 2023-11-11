638c81a7f7cde2674c6404e79a0471c75bc4fb77
stella-vir <stellavir11@gmail.com>
2023-11-11 18:24:49 +0200

    recursion

--- day_7.py
+++ day_7.py
@@ -1,5 +1,4 @@
 import sys 
-from collections import defaultdict, deque
 
 inputs = '''
 $ cd /
@@ -10,11 +9,11 @@ dir a
 dir d
 $ cd a
 $ ls
-dir e
+dir a
 29116 f
 2557 g
 62596 h.lst
-$ cd e
+$ cd a
 $ ls
 584 i
 $ cd ..
@@ -28,90 +27,110 @@ $ ls
 '''
 
 '''
-$ cd / $ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e '' cd ..
-{a: e, f, g, h}
-
-
 584 + 94853 -f (size 29116), g (size 2557), and h.lst (size 62596), plus file i(584) + 24933642 = 48381165
 95437 (94853 + 584) < 100000
-'''
-inputs += '$ ls'
 
-inputs = inputs.strip().split('\n')
-# inputs = open('day_7.txt').read().strip().split('\n')
+                    /
+        a               14848514   8504156          d
+    a 29116 2557 62596                      4060174 8033020 5626152 7214296
+584
+
+cd ..
+7214296 d 584 a 62596 2557 29116 a d 8504156 14848514 a /
+
+base case:
+recursion: 
+'''
+
+
+
+# inputs = inputs.strip().split('\n')
+inputs = open('day_7.txt').read().strip().split('\n')
+
+inputs = [line for line in inputs if '$ ls' not in line and '$ cd ..' not in line]
+
+# with open('test.txt', 'w') as output:
+#     output.write('\n'.join(inputs))
 
 # print(inputs)
 
+def test():
+    test_tar = 'tbj'
+    failure = 'test not passed '
+    success = 'test passed '
+    for line in inputs:
+        if test_tar in line:
+            print(line)
 
-def test(folders):
-    test_sentence = 'test not passed 4 sub folders'
-    if len(folders) < 4:
-        print(test_sentence, len(folders))
+
+
+
+def parse(line):
+    if '$ cd' in line:
+        _, _, sub_folder = line.split(' ')
+        return sub_folder
+    elif 'dir' in line:
+        _, sub_dir = line.split(' ')
+        return sub_dir
+    elif line[0].isdigit():
+         num, _ = line.split(' ')
+         num = int(num)
+         return num 
+    
+    
+
+def recursive(r, bucket, dir_folders, cd_folders, total):
+    threshold = 100000
+    sub_sum = 0
+
+    # dir: [wtmcqwgp, tbj, mdh, hzl]
+    # cd: [tbj, stcgrs, wtmcqwgp, tbj, mdh, hzl]
+
+    print('line, bucket', r, bucket)
+
+    if r > 0:
+        while not '$ cd' in inputs[r]:
+            val = parse(inputs[r])
+            print('val', val, inputs[r])
+            if not isinstance(val, int):
+                print('bucket val pop', val, bucket[val], dir_folders, cd_folders)
+                l = r + 1
+                dir_folders.append(val)
+                if l:
+                    cd_next = cd_folders[cd_folders.index(val) + 1]
+                    dir_next = dir_folders[dir_folders.index(val) + 1]
+                    if dir_next == cd_next:
+                        val = bucket.pop(val, None)
+                else:
+                    val = bucket.pop(val, None)
+            sub_sum += val
+            r -= 1
+        key = parse(inputs[r])
+        cd_folders.append(key)
+        if sub_sum < threshold:
+            total += sub_sum
+            print('new total', sub_sum, total)
+        bucket[key] = sub_sum
+        r -= 1
+        recursive(r, bucket, dir_folders, cd_folders, total)
     else:
-        print('test passed with 4 sub folders')
+        print('final', total)
+        return total
+        
 
-def parse(inp):
-    filtered_inp = []
-    for i, line in enumerate(inp):
-        if '$ ls' not in line and '$ cd ..' not in line:
-            filtered_inp.append(line)
-    inp = filtered_inp
-
-    folders = defaultdict(list)
-    name = None
-    for i, line in enumerate(inp):
-        if 'cd' in line:
-            name = line.split(' ')[-1]
-            folders[name] = []
-        elif 'dir' in line:
-            sub_folder = line.split(' ')[-1]
-            folders[name].append(sub_folder)
-        elif line[0].isdigit():
-            num, _ = line.split(' ')
-            num = int(num)
-            folders[name].append(num)
-
-    return folders
-
-def bfs(graph):
-    print(graph)
+def get_res():
+    last = len(inputs) - 1
+    dir_folders = []
+    cd_folders = []
+    bucket = {}
     total = 0
-    start = '/'
-    visited = set()
-    queue = deque()
+    
+    total = recursive(last, bucket, dir_folders, cd_folders, total)
+    return total
 
-    queue.append(start)
-
-    while queue:
-        key = queue.popleft()
-        if key not in visited:
-            visited.add(key)
-
-            for neighbor in graph[key]:
-                if neighbor not in visited:
-                    print(neighbor)
-                    queue.append(neighbor)
-
-
-
-def get_res(inp):
-    folders = parse(inp)
-    test(folders)
-    total = bfs(folders)
-
-if __name__ == "__main__":
-    res = get_res(inputs)
+if __name__ == '__main__':
+    res = get_res()
     print(res)
 
-
-
-
-
-
+    test()
 
