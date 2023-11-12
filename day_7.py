e275cce0ab124c6537252933474a2df65f4d9a20
stella-vir <stellavir11@gmail.com>
2023-11-12 13:26:17 +0200

    deletion works

--- day_7.py
+++ day_7.py
@@ -28,7 +28,7 @@ $ ls
 
 '''
 584 + 94853 -f (size 29116), g (size 2557), and h.lst (size 62596), plus file i(584) + 24933642 = 48381165
-95437 (94853 + 584) < 100000
+-> 95437 (94853 + 584) < 100000
 
                     /
         a               14848514   8504156          d
@@ -39,13 +39,15 @@ cd ..
 7214296 d 584 a 62596 2557 29116 a d 8504156 14848514 a /
 
 base case:
-recursion: 
+recursion:
+
+-> 1243729
 '''
 
 
 
-# inputs = inputs.strip().split('\n')
-inputs = open('day_7.txt').read().strip().split('\n')
+inputs = inputs.strip().split('\n')
+# inputs = open('day_7.txt').read().strip().split('\n')
 
 inputs = [line for line in inputs if '$ ls' not in line and '$ cd ..' not in line]
 
@@ -58,9 +60,9 @@ def test():
     test_tar = 'tbj'
     failure = 'test not passed '
     success = 'test passed '
-    for line in inputs:
-        if test_tar in line:
-            print(line)
+    # for line in inputs:
+    #     if test_tar in line:
+            # print(line)
 
 
 
@@ -79,40 +81,23 @@ def parse(line):
     
     
 
-def recursive(r, bucket, dir_folders, cd_folders, total):
+def recursive(r, bucket, total):
     threshold = 100000
     sub_sum = 0
-
-    # dir: [wtmcqwgp, tbj, mdh, hzl]
-    # cd: [tbj, stcgrs, wtmcqwgp, tbj, mdh, hzl]
-
-    print('line, bucket', r, bucket)
 
     if r > 0:
         while not '$ cd' in inputs[r]:
             val = parse(inputs[r])
-            print('val', val, inputs[r])
             if not isinstance(val, int):
-                print('bucket val pop', val, bucket[val], dir_folders, cd_folders)
-                l = r + 1
-                dir_folders.append(val)
-                if l:
-                    cd_next = cd_folders[cd_folders.index(val) + 1]
-                    dir_next = dir_folders[dir_folders.index(val) + 1]
-                    if dir_next == cd_next:
-                        val = bucket.pop(val, None)
-                else:
-                    val = bucket.pop(val, None)
+                val = bucket[val]
             sub_sum += val
             r -= 1
         key = parse(inputs[r])
-        cd_folders.append(key)
         if sub_sum < threshold:
             total += sub_sum
-            print('new total', sub_sum, total)
         bucket[key] = sub_sum
         r -= 1
-        recursive(r, bucket, dir_folders, cd_folders, total)
+        recursive(r, bucket, total)
     else:
         print('final', total)
         return total
@@ -120,12 +105,10 @@ def recursive(r, bucket, dir_folders, cd_folders, total):
 
 def get_res():
     last = len(inputs) - 1
-    dir_folders = []
-    cd_folders = []
     bucket = {}
     total = 0
     
-    total = recursive(last, bucket, dir_folders, cd_folders, total)
+    total = recursive(last, bucket, total)
     return total
 
 if __name__ == '__main__':
